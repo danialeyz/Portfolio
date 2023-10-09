22d4000c63cb52b72c7b9a34760ee66df92015d5
danialeyz <danialeyvaziiii@gmail.com>
2023-10-10 02:16:06 +0330

    mobile responsive fixed

--- js/main.js
+++ js/main.js
@@ -2,11 +2,14 @@
 const menuIcon = document.querySelector("#menu-icon");
 const menuItems = document.querySelector("#menu-items");
 const scrollToTopButton = document.querySelector("#scroll-top");
+const section6Cards = document.querySelector(".section-6-cards-container-1");
+
 let isOpen = false;
 
 // EVENT LISTENERS
 menuIcon.addEventListener("click", menuViewer);
 window.addEventListener("scroll", scrollButton);
+section6Cards.addEventListener("click", section6Scroll);
 
 // for hide and unhide navigation menu
 function menuViewer() {
@@ -26,4 +29,19 @@ function scrollButton() {
   } else {
     scrollToTopButton.style.display = "block";
   }
+}
+
+// For section 6 scroll
+
+function section6Scroll(e) {
+  // for getting pointer target element classlist
+  const className = [...e.target.classList];
+  const item = e.target;
+
+  // right
+  if (className[0] == "right") {
+    document.querySelector(".section-6-cards-container-2").scrollLeft += 400;
+  } else if (className[0] == "left") {
+    document.querySelector(".section-6-cards-container-2").scrollLeft -= 400;
+  } // Left
 }
